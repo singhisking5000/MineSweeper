88e14ff7d7ecc66cf3c1a1e81fe67b1bea071696
singhisking5000 <73306374+singhisking5000@users.noreply.github.com>
2026-04-21 16:58:51 +0000

    get sizing right
    Co-authored-by: CheeseBig0 <CheeseBig0@users.noreply.github.com>

--- src/main/java/com/example/Client.java
+++ src/main/java/com/example/Client.java
@@ -16,9 +16,10 @@ public class Client
     private static JLabel top;
     private static JLabel bottom;
     private static JPanel board;
+    private static int size = 20;
     private static int[][] tiles = {
-            {0, 0, 0, 0, 0, 0, 0, 0, 0},
             {0, 0, 1, 0, 0, 0, 1, 0, 0},
+            {0, 1, 0, 0, 0, 0, 0, 1, 0},
             {0, 0, 1, 0, 0, 0, 1, 0, 0},
             {0, 0, 0, 0, 1, 0, 0, 0, 0},
             {0, 1, 0, 0, 0, 0, 0, 1, 0},
@@ -38,6 +39,9 @@ public class Client
     private static void setupGUI()
     {
         f = new JFrame();
+        f.setLocation(500,500);
+        f.setPreferredSize(new Dimension(size*cols, size*rows));
+        f.setSize(new Dimension(size*cols, size*rows));
         top = new JLabel();
         bottom = new JLabel();
         board = new JPanel();
@@ -54,7 +58,7 @@ public class Client
 
         try {
             for(int x = 0; x < tiles.length; x++){
-                for(int y = 0; y < tiles[x].length; x++){
+                for(int y = 0; y < tiles[x].length; y++){
                     ImageIcon img;
                     img = new ImageIcon(ImageIO.read(new File("src/main/java/com/example/Bomb.png"))); // default to a bomb! :)
                     if(tiles[x][y] != 1) // If we're not a bomb...
@@ -62,7 +66,7 @@ public class Client
                         switch (countNearbyBombs(x,y)) // pick a picture
                         {
                             case 1:
-                                img = new ImageIcon(ImageIO.read(new File("src/main/java/com/example/OneTile.png")));
+                                img = new ImageIcon((ImageIO.read(new File("src/main/java/com/example/OneTile.png"))).getScaledInstance(size,size, Image.SCALE_SMOOTH));
                                 break;
                             case 2:
                                 img = new ImageIcon(ImageIO.read(new File("src/main/java/com/example/TwoTile.png")));
